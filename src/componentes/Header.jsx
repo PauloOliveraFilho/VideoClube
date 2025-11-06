dacfec34c2ad6310e52f1d6b90b62ff9429aa258
PauloOliveraFilho <paulofilhomarcondes@gmail.com>
2025-11-06 15:50:17 -0300

    v1.3.3 - Estrutura do header alterada

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -2,16 +2,34 @@ import "../styles/Header.css";
 
 function Header() {
   return (
-    <header>
-      <div className="header-logo">
-        <img></img>
-      </div>
-      <div>
-        <ul className="header-options">
-          <li>Home</li>
-          <li>Sobre nós</li>
-          <li>Entrar</li>
-        </ul>
+    <header className="site-header">
+      <div className="header-container">
+        <div className="header-logo">
+          <a href="/" className="header-brand" aria-label="Página inicial">
+            <span className="brand-mark">VC</span>
+            <span className="brand-name">VideoClube</span>
+          </a>
+        </div>
+
+        <nav aria-label="Navegação principal" className="header-right">
+          <ul className="header-options">
+            <li>
+              <a className="header-link" href="#">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="header-link" href="#">
+                Sobre nós
+              </a>
+            </li>
+            <li>
+              <a className="header-link" href="#">
+                Entrar
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );

20ad85b6d66112d6edf39f8fcaed448b5032d4a2
PauloOliveraFilho <paulofilhomarcondes@gmail.com>
2025-11-07 12:04:19 -0300

    v1.13.0 - Alterado a estrutura do App.jsx para adicionar as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
+import AboutUs from "./pages/AboutUs.jsx";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Home />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<AboutUs />} />
+        </Routes>
+      </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 

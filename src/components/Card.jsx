06bed0039e515b4191a729571d40e0e5f1183053
PauloOliveraFilho <paulofilhomarcondes@gmail.com>
2025-11-07 10:56:20 -0300

    v1.11.0 - Adicionado a estrutura html base do card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,15 @@
-function Card() {
-  return <div></div>;
+function Card({
+  name = "Placeholder",
+  description = "This is a placeholder card.",
+  imageUrl = "https://placehold.co/150",
+}) {
+  return (
+    <div className="card">
+      <img alt="Team member picture" src={imageUrl}></img>
+      <h3>{name}</h3>
+      <p>{description}</p>
+    </div>
+  );
 }
 
 export default Card;

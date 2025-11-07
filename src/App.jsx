ed9768e3fe27900c6bf8ea7e13bc3f0c8b93a3ab
PauloOliveraFilho <paulofilhomarcondes@gmail.com>
2025-11-07 11:12:26 -0300

    v1.11.2 - Arrumado caminho errado no import do Header e Footer em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import Header from "./componentes/Header.jsx";
-import Footer from "./componentes/Footer.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
 
 function App() {

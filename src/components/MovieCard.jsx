33762cc9e628c7c45d9ed1c7065b06cf37c9ed00
PauloOliveraFilho <paulofilhomarcondes@gmail.com>
2025-11-08 00:02:20 -0300

    v1.19.0 - Criação da página de detalhes do filme selecionado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,30 +1,17 @@
 import { useState } from "react";
 import "../styles/components/MovieCard.css";
+import { Link } from "react-router-dom";
+import moviesData from "../data/moviesData.json";
 
 function MovieCard() {
-  const [movies, setMovies] = useState([
-    {
-      title: "Titulo do Filme",
-      img: "Imagem",
-      caption: "Poster do filme",
-      description: "Descrição do filme",
-      rating: "1.0",
-    },
-    {
-      title: "Titulo do Filme",
-      img: "Imagem",
-      caption: "Poster do filme",
-      description: "Descrição do filme",
-      rating: "2.0",
-    },
-    {
-      title: "Titulo do Filme",
-      img: "Imagem",
-      caption: "Poster do filme",
-      description: "Descrição do filme",
-      rating: "3.0",
-    },
-  ]);
+  const [movies, setMovies] = useState(moviesData);
+
+  function formatCurrency(amount) {
+    return amount.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  }
 
   return (
     <section className="catalog-section">
@@ -32,19 +19,24 @@ function MovieCard() {
       <div className="movies-grid">
         {movies.map((movie, idx) => {
           return (
-            <article key={idx} className="movie-card">
-              <h3 className="movie-title">{movie.title + " " + (idx + 1)}</h3>
-              <figure>
-                <div className="movie-poster">
-                  {movie.img + " " + (idx + 1)}
-                </div>
-                <figcaption>{movie.caption + " " + (idx + 1)}</figcaption>
-              </figure>
-              <p className="movie-desc">
-                {movie.description + " " + (idx + 1)}
-              </p>
-              <p className="movie-rating">Nota: {movie.rating}</p>
-            </article>
+            <Link to={`/filme/${idx}`} key={idx} className="movie-link">
+              <article key={idx} className="movie-card">
+                <h3 className="movie-title">{movie.title + " " + (idx + 1)}</h3>
+                <figure>
+                  <div className="movie-poster">
+                    {movie.src + " " + (idx + 1)}
+                  </div>
+                  <figcaption>{movie.caption + " " + (idx + 1)}</figcaption>
+                </figure>
+                <p className="movie-desc">
+                  {movie.description + " " + (idx + 1)}
+                </p>
+                <p className="movie-rating">Nota: {movie.rating}</p>
+                <p className="movie-price">
+                  Preço: {formatCurrency(movie.price)}
+                </p>
+              </article>
+            </Link>
           );
         })}
       </div>
